d4e4f280d3a1d88a9cfc23e47b56990460f4d606
matsuyama.m <matsuyama.m@vstone.co.jp>
2019-11-21 15:07:58 +0900

    rosserial接続エラーへの対応

--- src/joycon.cpp
+++ src/joycon.cpp
@@ -39,7 +39,7 @@ JoyCtrlMegarover::JoyCtrlMegarover():
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("rover_twist", 1);
 
 
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &JoyCtrlMegarover::joyCallback, this);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 1, &JoyCtrlMegarover::joyCallback, this);
 
 }
 
@@ -61,6 +61,14 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "joycon");
   JoyCtrlMegarover joy_ctrl_megarover;
+
+	ros::NodeHandle n;
+
+	ros::Rate r(10);
+  while(n.ok()){
+
+  	ros::spinOnce();
+		r.sleep();
+	}
   
-  ros::spin();
 }
