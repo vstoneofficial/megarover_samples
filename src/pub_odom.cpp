2a3d78c0058f878f3a2258d51fec3db4be2f00c3
matsuyama.m <matsuyama.m@vstone.co.jp>
2019-11-21 15:14:57 +0900

    メガローバーのxacroモデルを追加

--- src/pub_odom.cpp
+++ src/pub_odom.cpp
@@ -65,7 +65,7 @@ int main(int argc, char** argv){
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time;
     odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
+    odom_trans.child_frame_id = "base_footprint";
 
     odom_trans.transform.translation.x = x;
     odom_trans.transform.translation.y = y;
